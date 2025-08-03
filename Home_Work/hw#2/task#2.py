1ca02d8d2def3ae1d71750197c645e475a823f00
Bohdan Voitov <bohdan.voiyov@gmail.com>
2025-08-03 18:13:39 +0300

    done_HW2_task#2

---
+++ Home_Work/hw#2/task#2.py
@@ -0,0 +1,7 @@
+a = int(input("Введіть ціле число a: "))
+b = int(input("Введіть ціле число b: "))
+x = int(input("Введіть ціле число x: "))
+
+product = a * b * x
+
+print("Добуток чисел:", product)
