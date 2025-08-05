b8e65e5de57a80821e3c0858454df209c4102aca
Bohdan Voitov <bohdan.voiyov@gmail.com>
2025-08-05 12:32:24 +0300

    done_HW3_task#1

---
+++ Home_Work/hw#3/task#1.py
@@ -0,0 +1,8 @@
+my_name = "Богдан"
+
+user_name = input("Введіть ваше ім'я: ")
+
+if user_name == my_name:
+    print("Ого! У нас однакові імена!")
+else:
+    print("Приємно познайомитись, " + user_name + "!")
