102a73bb601f5e4af52891372e8650cf4f2eac5b
Bohdan Voitov <bohdan.voiyov@gmail.com>
2025-08-16 13:06:57 +0300

    done_HW7_task5

---
+++ Home_Work/hw7/task5.py
@@ -0,0 +1,10 @@
+text = "apple banana apple orange banana apple orange grape grape grape"
+
+words = text.split()
+
+word_count = {}
+
+for word in words:
+    word_count[word] = word_count.get(word, 0) + 1
+
+print(word_count)
