9c616075d38854702ba9b48617537260c3222bd6
Bohdan <bohdan.voiyov@gmail.com>
2025-08-28 19:47:20 +0300

    f2

--- Test/git_test/test1.py
+++ Test/git_test/test1.py
@@ -14,3 +14,24 @@ def find_even_numbers_comprehension(numbers):
 my_list = [11, 12, 13, 14, 15, 16]
 even_list = find_even_numbers_comprehension(my_list)
 print(even_list) 
+
+
+# 2
+
+def f1(x):
+    return x**2 + 2 * x + 1
+
+
+def f2(x):
+    import math
+
+    return math.sin(x) + math.cos(x)
+
+
+print(f"{'x':>6} | {'f1(x)':>10} | {'f2(x)':>10}")
+print("-" * 32)
+
+x = -5.0
+while x <= 5.0:
+    print(f"{x:6.2f} | {f1(x):10.4f} | {f2(x):10.4f}")
+    x += 0.5
